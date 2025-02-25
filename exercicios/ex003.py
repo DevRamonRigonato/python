72c5ef1f6dfa42c692c4603c7658b8b9926f6f2d
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-02-25 13:05:13 -0300

    somando dois números

---
+++ exercicios/ex003.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digita um numero '))
+num2 = int(input('Digita outra numero '))
+soma = (num1 + num2)
+
+#print('A soma entre', num1,'e', num2, 'vale', soma) 
+
+print('A soma entre {} e {} vale {}'.format(num1, num2 , soma))
