d9dc2b37d4f97003b8db82692b9b958b0c114763
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-02-27 11:09:24 -0300

    d008 - Conversão de metros em cm e mm

---
+++ desafios/d008.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um número: '))
+centimetros = num * 100
+milimetros = num * 1000
+
+print('O valor convertido em centímetro é: {}'.format(centimetros))
+print('O valor convertido em milímetros é: {}'.format(milimetros))
