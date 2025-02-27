13c4d33b79a17ec54bdf7ccea1c670fa65c866ea
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-02-27 10:29:32 -0300

    d007 - Calcule a média de um aluno

---
+++ desafios/d007.py
@@ -0,0 +1,6 @@
+num1 = float(input('Digite a primeira nota: '))
+num2 = float(input('Digite a segunda nota: '))
+
+média_final = (num1 + num2) / 2
+
+print('A média do aluno é: {}'.format(média_final))
