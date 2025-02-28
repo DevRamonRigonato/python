012c99cafce3e7ef7a889b31a6a0ee569c498a8e
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-02-28 11:25:53 -0300

    d009 - Criando uma Taboada

---
+++ desafios/d009.py
@@ -0,0 +1,33 @@
+#taboada = num * 1, num * 2, num * 3, num * 4, num * 5, num * 6, num * 7, num * 8, num * 9, num * 10
+#print('resultado é: {}'.format(taboada))
+
+num = int(input('Digite um número: '))
+linha1 =  num * 1
+linha2 =  num * 2
+linha3 =  num * 3
+linha4 =  num * 4
+linha5 =  num * 5
+linha6 =  num * 6
+linha7 =  num * 7
+linha8 =  num * 8
+linha9 =  num * 9
+linha10 =  num * 10
+
+
+print('0 X 1 = {}'.format(linha1))
+print('0 X 2 = {}'.format(linha2))
+print('0 X 3 = {}'.format(linha3))
+print('0 X 4 = {}'.format(linha4))
+print('0 X 5 = {}'.format(linha5))
+print('0 X 6 = {}'.format(linha6))
+print('0 X 7 = {}'.format(linha7))
+print('0 X 8 = {}'.format(linha8))
+print('0 X 9 = {}'.format(linha9))
+print('0 X 10 = {}'.format(linha10))
+
+
+
+
+
+
+
