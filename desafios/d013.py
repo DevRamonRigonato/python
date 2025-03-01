ca40fd84981de9ba877a7f0f1ab60293f0297377
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-01 20:23:41 -0300

    d013 - Ajuste salarial

---
+++ desafios/d013.py
@@ -0,0 +1,7 @@
+funcionario = float(input('Digite o salário atual '))
+salario_atual = funcionario * 0.15
+novo_salario = funcionario + salario_atual
+
+print('Salário atual: {}'.format(funcionario))
+print('Aumento de 15%: {}'.format(salario_atual))
+print('Novo salário é: R$ {}'.format(novo_salario))
