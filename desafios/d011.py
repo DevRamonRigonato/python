6eabf5e3e56db5d8211cb0dbc3712f7209d0335e
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-01 19:25:09 -0300

    d011.py Calculando área da parede e pintura

---
+++ desafios/d011.py
@@ -0,0 +1,7 @@
+altura = float(input('Digite a altura: '))
+largura = float(input('Digite a largura: '))
+
+area_da_parede = (altura * largura) / 2
+
+print('A quantidade será de: {} litros de tinta'.format(area_da_parede))
+
