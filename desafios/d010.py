fd0aa49f1771233704505e895b4a8b3fe70cb204
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-02 20:14:04 -0300

    Pintando Parede - correcao

--- desafios/d010.py
+++ desafios/d010.py
@@ -1,4 +1,9 @@
-carteira = float(input('Quantos reais você tem? '))
-dolar = carteira / 5.85
+#carteira = float(input('Quantos reais você tem? '))
+#dolar = carteira / 5.85
 
-print('Você pode comprar apenas: {:.3f} dolares'.format(dolar))
+#print('Você pode comprar apenas: {:.2f} dolares'.format(dolar))
+
+real = float(input('Quanto de dinheiro você tem na carteira? R$'))
+dolar = real / 5.85
+
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
--- desafios/d011.py
+++ desafios/d011.py
@@ -1,7 +1,15 @@
-altura = float(input('Digite a altura: '))
-largura = float(input('Digite a largura: '))
+#altura = float(input('Digite a altura: '))
+#largura = float(input('Digite a largura: '))
 
-area_da_parede = (altura * largura) / 2
+#area_da_parede = (altura * largura) / 2
 
-print('A quantidade será de: {} litros de tinta'.format(area_da_parede))
+#print('A quantidade será de: {} litros de tinta'.format(area_da_parede))
 
+largura = float(input('Largua da parede: '))
+altura = float(input('Altura da parede: '))
+área = largura * altura
+
+print('Sua parede tem a dimensão de {}x{}e sua área é de {}mm².'.format(largura, altura, área))
+
+tinta = área / 2
+print('Para pintar essa parede, você precisará de {}L de tinta.'.format(tinta))
