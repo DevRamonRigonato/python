213592f5c8154ff9a119f34448f6c4832237d792
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-02 18:38:23 -0300

    Média Aritmética - melhorado

--- desafios/d007.py
+++ desafios/d007.py
@@ -1,6 +1,12 @@
-num1 = float(input('Digite a primeira nota: '))
-num2 = float(input('Digite a segunda nota: '))
+#num1 = float(input('Digite a primeira nota: '))
+#num2 = float(input('Digite a segunda nota: '))
 
-média_final = (num1 + num2) / 2
+#média_final = (num1 + num2) / 2
 
-print('A média do aluno é: {}'.format(média_final))
+#print('A média do aluno é: {}'.format(média_final))
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+média = (n1 + n2) / 2
+
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n1, n2, média))
