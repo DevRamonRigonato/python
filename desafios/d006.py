46257531ca9df141c4ece280a251adde9fefb451
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-02 18:09:50 -0300

    Outro exemplo de algoritimo

--- desafios/d006.py
+++ desafios/d006.py
@@ -1,6 +1,10 @@
-num = int(input('Digite um número: '))
-dobro = num * 2
-triplo = num * 3 
-raiz_quadrada = num ** 0.5
+#num = int(input('Digite um número: '))
+#dobro = num * 2
+#triplo = num * 3 
+#raiz_quadrada = num ** 0.5 # (1/2)
 
-print('Dobro: {} Triplo: {} Raiz: {}'.format(dobro, triplo, raiz_quadrada))
+#print('Dobro: {} Triplo: {} \nRaiz: {:.2f}'.format(dobro, triplo, raiz_quadrada))
+
+n = int(input('Digite um número: '))
+print('O dobro de {} vale {}.'.format(n, (n*2)))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*3), n, (n**(1/2))))
