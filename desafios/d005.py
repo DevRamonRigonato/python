27fb7085052f5e05c294815352487ae9d77d711c
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-02 17:20:49 -0300

    Ante e Suces - 2 maneira de fazer

--- desafios/d005.py
+++ desafios/d005.py
@@ -1,5 +1,8 @@
-num = int(input('Digite um número inteiro: '))
-antecessor = num - 1
-sucessor = num + 1
+#num = int(input('Digite um número inteiro: '))
+#antecessor = num - 1
+#sucessor = num + 1
 
-print('O antecessor de {} é {} e o sucessor é {}'.format(num, antecessor, sucessor))
+#print('O antecessor de {} é {} e o sucessor é {}'.format(num, antecessor, sucessor))
+
+n = int(input('Digite um número: '))
+print('Analizando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
