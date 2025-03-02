821d5b3b9ec664494ddbe543c088dd37d6242fc7
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-02 19:38:05 -0300

    Tabuada correçao

--- desafios/d009.py
+++ desafios/d009.py
@@ -1,31 +1,43 @@
 #taboada = num * 1, num * 2, num * 3, num * 4, num * 5, num * 6, num * 7, num * 8, num * 9, num * 10
 #print('resultado é: {}'.format(taboada))
 
-num = int(input('Digite um número: '))
-linha1 =  num * 1
-linha2 =  num * 2
-linha3 =  num * 3
-linha4 =  num * 4
-linha5 =  num * 5
-linha6 =  num * 6
-linha7 =  num * 7
-linha8 =  num * 8
-linha9 =  num * 9
-linha10 =  num * 10
+#num = int(input('Digite um número: '))
+#linha1 =  num * 1
+#linha2 =  num * 2
+#linha3 =  num * 3
+#linha4 =  num * 4
+#linha5 =  num * 5
+#linha6 =  num * 6
+#linha7 =  num * 7
+#linha8 =  num * 8
+#linha9 =  num * 9
+#linha10 =  num * 10
 
 
-print('0 X 1 = {}'.format(linha1))
-print('0 X 2 = {}'.format(linha2))
-print('0 X 3 = {}'.format(linha3))
-print('0 X 4 = {}'.format(linha4))
-print('0 X 5 = {}'.format(linha5))
-print('0 X 6 = {}'.format(linha6))
-print('0 X 7 = {}'.format(linha7))
-print('0 X 8 = {}'.format(linha8))
-print('0 X 9 = {}'.format(linha9))
-print('0 X 10 = {}'.format(linha10))
+#print('0 X 1 = {}'.format(linha1))
+#print('0 X 2 = {}'.format(linha2))
+#print('0 X 3 = {}'.format(linha3))
+#print('0 X 4 = {}'.format(linha4))
+#print('0 X 5 = {}'.format(linha5))
+#print('0 X 6 = {}'.format(linha6))
+#print('0 X 7 = {}'.format(linha7))
+#print('0 X 8 = {}'.format(linha8))
+#print('0 X 9 = {}'.format(linha9))
+#print('0 X 10 = {}'.format(linha10))
 
-
+num = int(input('Digite um número para ver sua tabuada: '))
+print('-' * 12)
+print('{} x {:} = {}'.format(num, 1, num*1))
+print('{} x {:} = {}'.format(num, 2, num*2))
+print('{} x {:} = {}'.format(num, 3, num*3))
+print('{} x {:} = {}'.format(num, 4, num*4))
+print('{} x {:} = {}'.format(num, 5, num*5))
+print('{} x {:} = {}'.format(num, 6, num*6))
+print('{} x {:} = {}'.format(num, 7, num*7))
+print('{} x {:} = {}'.format(num, 8, num*8))
+print('{} x {:} = {}'.format(num, 9, num*9))
+print('{} x {:} = {}'.format(num, 10, num*10))
+print('-' * 12)
 
 
 
