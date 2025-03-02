0c060645907979152d98a3afd3527590b57163f9
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-02 20:54:04 -0300

    calculando descontos - corrigido

--- desafios/d012.py
+++ desafios/d012.py
@@ -1,6 +1,11 @@
-produto = float(input('Digite o produto '))
-liquidacao = produto * 0.05 
-desconto_final = produto - liquidacao
+#produto = float(input('Digite o produto '))
+#liquidacao = produto * 0.05 
+#desconto_final = produto - liquidacao
 
-print('desconto de: {}'.format(liquidacao))
-print('Preço final com desconto: R${}'.format(desconto_final))
+#print('desconto de: {:.2f}'.format(liquidacao))
+#print('Preço final com desconto: R${:.2f}'.format(desconto_final))
+
+preço = float(input('Qual é o preço do produto? '))
+novo = preço - (preço * 5 / 100)
+
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preço,novo))
