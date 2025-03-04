fccb68709e2bcd98109f32f05cf05a4d85ecba18
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-04 15:31:20 -0300

    ex009 - Quebrando um número

---
+++ exercicios/ex009.py
@@ -0,0 +1,11 @@
+'''from math import sqrt, floor,ceil,trunc
+num = float(input('Digite um número: '))
+inteira = sqrt(num)
+print('O número {} tem a parte inteira {}'.format(num,trunc(num)))'''
+
+'''num = float(input('Digite um valor: '))
+print('O número {} tem a parte inteira {}'.format(num, int(num)))'''
+
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
