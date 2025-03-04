7b3b506360b540e90fb5a9d9ab753efb603c1619
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-04 14:40:26 -0300

    ex008 - Utilizando Módulos

---
+++ exercicios/ex008.py
@@ -0,0 +1,19 @@
+#Importando o módulo inteiro
+#import math 
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+
+#Importando apenas uma parte do módulo
+#from math import sqrt, floor
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
+
+#import random
+#num = random.randint(1, 10)
+#print(num)
+
+
+import emoji
+print(emoji.emojize('Olá, Mundo :globe_showing_americas:', language='alias'))
