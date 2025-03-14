551805e593f8590d19ea8a93dec40bb321ec05a5
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-14 19:50:24 -0300

    ex031 - Cores no Terminal

---
+++ exercicios/ex031.py
@@ -0,0 +1,3 @@
+nome = 'Ramon'
+cores = {'limpa':'\033[41;46m', 'azul': '\033[34m]', 'amarelo': '\033[m', 'pretoebranco': '\033[7;30m'}
+print('Olá! Muito prazer em te conhecer, {}{}{}!!'.format(cores['pretoebranco'], nome, cores['limpa']))
