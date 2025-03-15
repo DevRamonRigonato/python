233af253f335a7a6585033aa040e5f6ab3d71aec
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-15 14:56:28 -0300

    aplicando cores

--- exercicios/ex030.py
+++ exercicios/ex030.py
@@ -1,52 +1,52 @@
-print('===== Prato Principal =====')
-print('[1] Strogonoff')
-print('[2] Carne de Panela')
-print('[3] Udon')
-prato = int(input('Escolha um número: '))
+print('===== \033[0;37;42mPrato Principal\033[0m =====')
+print('[1] \033[0;31mStrogonoff\033[0m')
+print('[2] \033[0;31mCarne de Panela\033[0m')
+print('[3] \033[0;31mUdon\033[0m')
+prato = int(input('\033[0;33mEscolha um número: \033[0m'))
 
-print('===== Bebida =====')
-print('[1] Coca-Cola')
-print('[2] Suco')
-print('[3] Milk-Shake')
-bebida = int(input('Escolha um número: '))
+print('===== \033[0;37;42mBebida\033[0m =====')
+print('[1] \033[0;31mCoca-Cola\033[0m')
+print('[2] \033[0;31mSuco\033[0m')
+print('[3] \033[0;31mMilk-Shake\033[0m')
+bebida = int(input('\033[0;33mEscolha um número: \033[0m'))
 
-print('===== Sobremesa =====')
-print('[1] Açai')
-print('[2] Pavê')
-print('[3] Bolo de Chocolate')
-sobremesa = int(input('Escolha um número: '))
+print('===== \033[0;37;42mSobremesa\033[0m =====')
+print('[1] \033[0;31mAçai\033[0m')
+print('[2] \033[0;31mPavê\033[0m')
+print('[3] \033[0;31mBolo de Chocolate\033[0m')
+sobremesa = int(input('\033[0;33mEscolha um número: \033[0m'))
 
-print('===== SEU PEDIDO FOI =====')
+print('===== \033[0;45mSEU PEDIDO FOI\033[0m =====')
 
 match prato:
     case 1:
-        print('Prato principal: Strogonoff')
+        print('Prato principal: \033[0;36mStrogonoff\033[0m')
     case 2:
-        print('Prato principal: Carne de Panela')
+        print('Prato principal: \033[0;36mCarne de Panela\033[0m')
     case 3:
-        print('Prato principal: Udon')    
+        print('Prato principal: \033[0;36mUdon\033[0m')    
     case _ :
-        print('Prato principal: Opção inválida')
+        print('Prato principal: \033[0;37;47mOpção inválida\033[0m')
 
 match bebida:
     case 1:
-        print('Bebida: Coca-Cola')
+        print('Bebida: \033[0;36mCoca-Cola\033[0m')
     case 2:
-        print('Bebida: Suco')
+        print('Bebida: \033[0;36mSuco\033[0m')
     case 3:
-        print('Bebida: Milk-Shake')
+        print('Bebida: \033[0;36mMilk-Shake\033[0m')
     case _:
-        print('Bebida: Opção inválida')
+        print('Bebida: \033[0;37;47mOpção inválida\033[0m')
 
 match sobremesa:
     case 1:
-        print('Sobremesa: Açai')
+        print('Sobremesa: \033[0;36mAçai\033[0m')
     case 2:
-        print('Sobremesa: Pavê')
+        print('Sobremesa: \033[0;36mPavê\033[0m')
     case 3:
-        print('Sobremesa: Bolo de Chocolate')
+        print('Sobremesa: \033[0;36mBolo de Chocolate\033[0m')
     case _:
-        print('Sobremesa: Opção inválida')
+        print('Sobremesa: \033[0;37;47mOpção inválida\033[0m')
 
  
 
