901f5a65a5ef72f61e8183a996cfe68ff740395a
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-16 13:44:58 -0300

    add cores

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -1,13 +1,13 @@
 print('-=-' * 20)
-print('Analisador de Triângulos') 
+print('\033[0;36mAnalisador de Triângulos\033[0m') 
 print('-=-' * 20)      
-r1 = float(input('Primeiro segmento: '))   
-r2 = float(input('Segundo segmento: '))     
-r3 = float(input('Terceiro segmento: '))    
+r1 = float(input('\033[0;33mPrimeiro segmento:\033[0m '))   
+r2 = float(input('\033[0;33mSegundo segmento: \033[0m'))     
+r3 = float(input('\033[0;33mTerceiro segmento: \033[0m'))    
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os seguimentos acima PODEM FORMAR triângulo!')
+    print('\033[0;32mOs seguimentos acima PODEM FORMAR triângulo!\033[0m')
 else:
-    print('Os segmentos acima NÃO PODEM formar triângulo!')
+    print('\033[0;31mOs segmentos acima NÃO PODEM formar triângulo!\033[0m')
 
  
