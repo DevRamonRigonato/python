3593a411748a6dd66b0f752f9b93f5337f849451
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-21 10:37:48 -0300

    ex033 - Correção

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -1,4 +1,4 @@
-nome = str(input('Digite o nome do aluno: '))
+'''nome = str(input('Digite o nome do aluno: '))
 p1 = float(input('Digite a nota da P1: '))
 p2 = float(input('Digite a nota da P2: '))
 media = (p1 + p2) / 2
@@ -12,7 +12,18 @@ elif media > 5.0 and 6.9:
 else:
     print('Média de 5.0: '
     'REPROVADO!')
-print(media)
+print(media)'''
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segudna nota: '))
+media = (n1 + n2) / 2
+
+if 7 > media >= 5:
+    print('O aluno está em RECUPERAÇÃO.')
+elif media < 5:
+    print('REPROVADO.')
+elif media >= 7:
+    print('O aluno está APROVADO.')
 
 
 
