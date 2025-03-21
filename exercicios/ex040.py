608368898b6886f2d719d27c12e6acc5f464e970
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-21 16:11:11 -0300

    ex040 - Analisando Triângulos v2.0

---
+++ exercicios/ex040.py
@@ -0,0 +1,12 @@
+r1 = int(input('Primeiro segmento: '))
+r2 = int(input('Segundo segmento: '))
+r3 = int(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2: # if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1: Forma mais intuitiva de entender!
+    if r1 == r2 == r3:
+        print('Os seguimentos acima PODEM FORMAR um triângulo EQUILÁTERO!')
+    elif r1 == r2 or r1 == r3 or r2 == r3:
+        print('Os seguimentos acima PODEM FORMAR um triângulo ISÓSCELES!')
+    elif r1 != r2 and r1 != r3 and r2 != r3:
+        print('Os segmentos acima PODEM FORMAR um triângulo ESCALENO!')
+else:
+    print('Não PODEM FORMAR triãngulo')
