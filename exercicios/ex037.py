08a4656679d9d73aafcb4927407551f1161354f3
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-25 21:23:16 -0300

    add cor

--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -2,9 +2,9 @@ n1 = int(input('Digite um número inteiro: '))
 n2 = int(input('Digite outro número inteiro: '))
 
 if n1 > n2:
-    print('O primeiro valor é maior')
+    print('\033[0;32mO primeiro valor é maior\033[m')
 elif n2 > n1:
-    print('O segundo valor é maior')
+    print('\033[0;32mO segundo valor é maior\033[m')
 else:
-    print('Não existe valor maior, os dois são iguais')
+    print('\033[0;31mNão existe valor maior, os dois são iguais\033[m')
 
