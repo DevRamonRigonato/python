e97228f3763b771dff6007b47dc4cdff9de2f989
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-03-31 11:15:25 -0300

    ex043 - Estrutura de repetição for

---
+++ exercicios/ex043.py
@@ -0,0 +1,27 @@
+'''for i in range(0, 105, 5):
+    print(i)
+print('FIM')'''
+
+'''n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('FIM')'''
+
+'''i = int(input('Início: '))
+f = int(input('Fim:'))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')'''
+
+'''for c in range(0, 100000):
+    n = int(input('Digite um valor: '))
+    if n == 3:
+        break
+print('FIM')'''
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
