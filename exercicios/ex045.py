35cc9dc786b78c9665e4fd8909b6cd6c04687f5d
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-04-01 10:02:08 -0300

    ex046 - Contagem de pares

---
+++ exercicios/ex045.py
@@ -0,0 +1,15 @@
+
+'''for i in range(1, 51):
+    if i % 2 == 0:
+        print(i)
+print('ACABOU')'''
+
+for i in range(2, 51, 2):
+    print('.', end='')
+    print(i, end=' ')
+print('ACABOU')
+    
+
+
+
+    
