48cce58e8d2ace8fafea5e1d63ff5fc1351ad317
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-04-10 11:13:31 -0300

    Create ex056.py

---
+++ exercicios/ex056.py
@@ -0,0 +1,26 @@
+media_idade = 0
+soma_idade = 0
+homemidade_velho = 0
+velhohomem = ''
+tot_mulher = 0
+for p in range(1, 5):
+    nome = str(input('Digite o nome: ')).strip()
+    idade = int(input('Digite a idade: '))
+    sexo = str(input('Digite o sexo [M/F]: ')).strip().upper()
+    media_idade += idade
+    if p == 1 and sexo in 'Mm':
+        homemidade_velho = idade
+        velhohomem = nome
+    if sexo in 'Mm' and idade > homemidade_velho:
+        homemidade_velho = idade
+        velhohomem = nome
+    if sexo in 'Ff' and idade < 20:
+        tot_mulher += 1 #tot_mulher = tot_mulher + 1
+
+soma_idade = media_idade / 4
+print('A média idade do grupo é {} anos'.format(soma_idade))
+print('O homem mais velho chama {} e tem {} anos de idade.'.format(velhohomem, homemidade_velho))
+print('Tem no total {} mulheres menores de idade.'.format(tot_mulher))
+
+
+
