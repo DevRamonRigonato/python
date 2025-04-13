b05bc555c6112c81dc86be9a3b44d2f3c0d86583
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-04-13 11:05:30 -0300

    Update ex46.py

--- exercicios/ex46.py
+++ exercicios/ex46.py
@@ -4,4 +4,4 @@ for i in range(1, 501, 2):
     if i % 3 == 0:
         contador += 1
         soma += i
-print('A soma de todos os {} valores solicitados é {}'.format(contador, soma))
+print('\033[0;32mA soma de todos os {} valores solicitados é {}\033[m'.format(contador, soma))
