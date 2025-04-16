bf6df2960b586eff18461d1575a05b7a07263326
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-04-16 15:07:41 -0300

    ex063 - Super Progressão Aritmética v3.0

--- exercicios/ex063.py
+++ exercicios/ex063.py
@@ -2,7 +2,14 @@ primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão da PA: '))
 termo = primeiro # mostra o termo
 cont = 1 # conta quantos termos foram
-while cont <= 10: 
-    print('{} >'.format(termo), end=' ')
-    termo += razao
-    cont += 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total: 
+        print('{} >'.format(termo), end=' ')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
