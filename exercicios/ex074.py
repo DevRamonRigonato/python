4c4e186e3f8a9686644305ac05217dcb322d86d4
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-04-29 13:13:15 -0300

    revisão

---
+++ exercicios/ex074.py
@@ -0,0 +1,18 @@
+'''a = 7
+b = "42"
+c = False
+d = 3.5
+e = '7.5'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))'''
+
+'''x = '10'
+y = 10
+resultado = int(x) + y
+print(resultado)'''
+
+
