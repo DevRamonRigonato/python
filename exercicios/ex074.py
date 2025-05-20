775e78e1556c669ae4fc505734bf6d3f67f2409a
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-05-20 11:33:44 -0300

    Update ex074.py

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -728,3 +728,29 @@ while n != 0:
             impar += 1
 print(f'Você digitou {par} números pares {impar} números ímpares!')'''
 
+'''sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print(f'Sexo {sexo} registrado com sucesso')'''
+
+
+from random import randint
+computador = randint(0, 10)
+print('''Sou um computador...
+Acabei de pensar em um número entre 0 e 10,
+Será que você consegue advinhar qual foi?''')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez. ')
+print(f'Acertou com {palpites} tentativas. Parabéns!')
+
+
