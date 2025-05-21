3ddf5d3d25470b67b2f50a0d038a325798f063f3
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-05-21 11:14:51 -0300

    Update ex074.py

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -734,7 +734,7 @@ while sexo not in 'MF':
 print(f'Sexo {sexo} registrado com sucesso')'''
 
 
-from random import randint
+"""from random import randint
 computador = randint(0, 10)
 print('''Sou um computador...
 Acabei de pensar em um número entre 0 e 10,
@@ -751,6 +751,46 @@ while not acertou:
             print('Mais... Tente mais uma vez.')
         elif jogador > computador:
             print('Menos... Tente mais uma vez. ')
-print(f'Acertou com {palpites} tentativas. Parabéns!')
+print(f'Acertou com {palpites} tentativas. Parabéns!')"""
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opção = 0
+while opção != 5:
+    print('''
+    [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair do programa
+''')
+    opção = int(input('>>>>> Qual é a sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print(f'A soma entre {n1} + {n2} é {soma}')
+    elif opção == 2:
+        produto = n1 * n2
+        print(f'O resultado de {n1} x {n2} é {produto}')
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print(f'Entre {n1} e {n2} o maior valor é {maior}')
+    elif opção == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente')
+    print('=-=' * 10)
+print('Fim do programa! Volte sempre!')
+
+   
+
+   
+         
 
 
