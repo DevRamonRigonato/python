83aa5e4bb4c13edeb0d5a1a439ce0633b303b98a
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-05-23 11:00:31 -0300

    Update ex074.py

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -796,3 +796,20 @@ while num != 999:
     num = int(input('Digite um número [999 para parar]: '))
 print(f'Você digitou {cont} números e a soma entre eles foi {soma}')'''
 
+'''resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print(f'Você digitou {quant} números é a média foi {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')'''
