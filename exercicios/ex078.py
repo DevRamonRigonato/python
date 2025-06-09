5b6829003e49094b773c1766fda7332f13103dab
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-06-09 16:49:01 -0300

    Update ex078.py

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -13,38 +13,3 @@ print(f'Os valores pares digitados foram ', end='')
 for n in núm:
     if n % 2 == 0:
         print(n, end=' ')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
