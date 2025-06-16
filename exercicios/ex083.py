c8e45b037a5e2752155ca9996ba39df7f89ecfb2
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-06-16 15:06:55 -0300

    ex083 - Valores únicos em uma Lista

--- exercicios/ex083.py
+++ exercicios/ex083.py
@@ -1,28 +1,16 @@
-x = 1 / 2 + 3 // 3 + 4 ** 2 
-print(x)
+listanum = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    num = int(input('Digite um valor: '))
+    if num in listanum: # Pode colocar not in tbm, porém precisa reorganizar as mensagens e o que é feito em cada bloco.
+        print('Valor duplicado! Não vou adicionar...')
+    else:
+        listanum.append(num)
+        print('Valor adicionado com sucesso...')
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+listanum.sort()
+print(f'Você digitou os valores {listanum}')
