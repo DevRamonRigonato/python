34a9cef82086cd1a83bcd0e7d941dcc206a0c4cd
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-07-14 09:47:50 -0300

    agora vai

--- exercicios/ex093.py
+++ exercicios/ex093.py
@@ -1,137 +1,23 @@
-'''matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-soma =  soma3c =  maiorvalor = 0
-for l in range(0, 3):# lendo a matriz os valores, essas 3 linhas vão preencher minha matriz
-    for c in range(0, 3):# lendo a matriz os valores, essas 3 linhas vão preencher minha matriz
-        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: ')) # lendo a matriz os valores, essas 3 linhas vão preencher minha matriz
-print('-=' * 30)
-for l in range(0, 3):
-    for c in range(0, 3):
-        if matriz[l][c] % 2 == 0:
-            soma += matriz[l][c]
-        if c == 2:
-            soma3c += matriz[l][c]                           # mostra a matriz na TELA!
-        if l == 1:
-            matriz[l][c] > maiorvalor
-            maiorvalor = matriz[l][c]
-        print(f'[{matriz[l][c]:^5}]', end='')
-    print()
-print('-=' * 30)
-print(f'A soma dos valores pares é {soma}')
-print(f'A soma dos valores da terceira coluna é {soma3c}')
-print(f'O mair valor da segunda linha é {maiorvalor}')'''
-
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-spar = scol = maior = 0
-for l in range(0, 3):
-    for c in range(0, 3):
+spar = mai = scol = 0
+for l in range(0, 3): # FOR de alimentação onde os valores serão colocados dentro da MATRIZ.
+    for c in range(0, 3): # FOR de alimentação onde os valores serão colocados dentro da MATRIZ.
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
-        if matriz[l][c] % 2 == 0:
-            spar += matriz[l][c]    
 print('-=' * 30)
-for l in range(0, 3):
-    for c in range(0, 3):
+for l in range(0, 3): # São estruturas de repetição para mostrar a estrutura da matriz na tela.
+    for c in range(0, 3): # São estruturas de repetição para mostrar a estrutura da matriz na tela.
         print(f'[{matriz[l][c]:^5}]', end='')
-    print()
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
+    print() # Tpda vez que terminar as colunas ele quebra pra poder fazer uma linha.
 print('-=' * 30)
 print(f'A soma dos valores pares é {spar}')
 for l in range(0, 3):
     scol += matriz[l][2]
-print(f'A soma dos valores da terceira coluna é {scol}')
+print(f'A soma dos valores da terceira coluna é {scol}.')
 for c in range(0, 3):
-    if c == 0: 
-        maior = matriz[1][c]
-    elif matriz[1][c] > maior:
-        maior = matriz[1][c]
-print(f'O maior velor da segunda linha é {maior}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if c == 0:
+        mai = matriz[1][c]
+    elif matriz[1][c] > mai:
+        mai = matriz[1][c]
+print(f'O maior valor da segunda linha é {mai}.')
