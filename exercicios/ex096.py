f43948a7babbb9467cc8a166254145d22675f7f2
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-07-18 16:13:34 -0300

    revisão

--- exercicios/ex096.py
+++ exercicios/ex096.py
@@ -82,7 +82,41 @@ for i in range(len(idades)):
 for nome in nomes:
     if len(nomes) <= 5:
         print(nome)'''
-num = []
-for i in range(0,5):
-    num = int(input(f'Digite um valor para a Posição: {i}: '))
-print(f'Você digitou os valores {num[i]}')
+
+
+'''listanum = []
+mai = men = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+print('-=' * 30)
+print(f'Os valores digitados foram: {listanum}')
+print(f'O maior valor digitado foi {mai} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == mai:
+        print(f'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {men} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}... ', end='')
+print()'''
+
+'''pares = []
+impares = []
+for c in range(7):
+    valor = int(input(f'Digite o {c+1}° valor: '))
+    if valor % 2 == 0:
+        pares.append(valor)
+    else:
+        impares.append(valor)
+print(f'Pares: {pares}')
+print(f'Ímpares: {impares}')'''
+
+
