7ccfbd651d58ffb7652934def4a1c544ff5598f7
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-07-30 12:13:06 -0300

    aprendendo dicionario

---
+++ exercicios/ex097.py
@@ -0,0 +1,18 @@
+#pessoas = {'nome': 'Ramon', 'sexo': 'M', 'idade': '26'}
+# ['peso'] = 95
+#pessoas ['altura'] = 1.69
+#pessoas ['nome'] = 'Rebeca'
+#del pessoas['sexo']
+#for k, v in pessoas.items():
+#    print(f'{k} = {v}')
+#print(pessoas.keys())
+#print(pessoas.values())
+#print(pessoas.items())
+#print(f'O {pessoas["nome"]} tem {pessoas["idade"]} e é do sexo {pessoas["sexo"]}')
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil[1] ['uf'])
