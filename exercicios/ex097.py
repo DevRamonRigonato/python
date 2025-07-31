d449e14f60b8bf7239c7d4a7c8d23d9253f884bf
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-07-31 11:25:40 -0300

    Aula completa Dicionario

--- exercicios/ex097.py
+++ exercicios/ex097.py
@@ -1,18 +1,25 @@
-#pessoas = {'nome': 'Ramon', 'sexo': 'M', 'idade': '26'}
-# ['peso'] = 95
-#pessoas ['altura'] = 1.69
-#pessoas ['nome'] = 'Rebeca'
-#del pessoas['sexo']
-#for k, v in pessoas.items():
-#    print(f'{k} = {v}')
-#print(pessoas.keys())
-#print(pessoas.values())
-#print(pessoas.items())
-#print(f'O {pessoas["nome"]} tem {pessoas["idade"]} e é do sexo {pessoas["sexo"]}')
+'''pessoas = {'Nome': 'Ramon', 'Idade': '26', 'Sexo': 'M'}
+pessoas ['Nome'] = 'Rebeca'
+pessoas ['Peso'] = 68
+pessoas ['Sexo'] = 'F'
+pessoas ['Idade'] = 24
+for k, v in pessoas.items():
+    print(f'{k} = {v}')'''
 
-brasil = []
+'''brasil = []
 estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
 estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
 brasil.append(estado1)
 brasil.append(estado2)
-print(brasil[1] ['uf'])
+
+print(brasil[0] ['sigla'])'''
+
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado ['uf'] = str(input('Unidade Federativa: '))
+    estado ['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}')
