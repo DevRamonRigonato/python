4420ba7f89ef40ec1e59633b928d224956d1b012
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-08-09 20:59:34 -0300

    ex102 - Unindo dicionários e listas

--- exercicios/ex102.py
+++ exercicios/ex102.py
@@ -5,7 +5,7 @@ while True:
     pessoa.clear()
     pessoa ['nome'] = str(input('Nome: '))
     while True:
-        pessoa ['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+        pessoa ['sexo'] = str(input('Sexo: [M/F]: ')).strip().upper()[0]
         if pessoa ['sexo'] in 'MF':
             break
         print('Erro! Por favor, digite apenas M ou F.')
@@ -14,26 +14,27 @@ while True:
     galera.append(pessoa.copy())
     resp = ' '
     while True:
-         resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0] 
-         if resp in 'SN':
-             break
-         print('Erro! Responda apenas S ou N.')
+        resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        print('Erro! Responda apenas S ou N.')
     if resp == 'N':
-        break
-print('-=' * 30)
-print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+            break
 media = soma / len(galera)
-print(f'B) A média de idade é de {media:5.2f} anos.')
-print('C) As mulheres cadastradas foram: ', end='')
+print('-=' * 30)
+print(f'A) Ao todo tem {len(galera)} pessoas cadastradas.')
+print(f'B) A média de idade é de {media:5.2f} anos.') 
+print(f'C) As mulheres cadastradas foram: ', end='')
 for p in galera:
-    if p['sexo'] == 'F':
-        print(f'{p["nome"]}', end=' ')
+     if p ['sexo'] == 'F':
+          print(f'{p["nome"]} ', end='')
 print()
-print('D) Lista das pessoas que estão acima da média: ', end='')
+print('D) A lista de pessoas que estão acima da média: ')
 for p in galera:
-    if p['idade'] >= media:
-        print('    ')
-        for k, v in p.items():
-            print(f'{k} = {v}; ', end='')
-        print()
-print('<< ENCERRADA >>')
+     if p ['idade'] >= media:
+          print('    ')
+          for k, v in p.items():
+              print(f'{k} = {v}; ', end='')  
+          print()
+print('<< ENCERRADO >>')      
+            
