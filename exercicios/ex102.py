a7af5d8e1cb4cb0a93b43330b7dc37a709594f94
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-08-10 19:13:55 -0300

    ex102 - Unindo dicionários e listas
    FINALIZADO!

--- exercicios/ex102.py
+++ exercicios/ex102.py
@@ -5,36 +5,35 @@ while True:
     pessoa.clear()
     pessoa ['nome'] = str(input('Nome: '))
     while True:
-        pessoa ['sexo'] = str(input('Sexo: [M/F]: ')).strip().upper()[0]
+        pessoa ['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
         if pessoa ['sexo'] in 'MF':
             break
-        print('Erro! Por favor, digite apenas M ou F.')
+        print('Erro! Por favor, digite novamente M ou F.')
     pessoa ['idade'] = int(input('Idade: '))
     soma += pessoa ['idade']
     galera.append(pessoa.copy())
     resp = ' '
     while True:
-        resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
+        resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
         if resp in 'SN':
             break
-        print('Erro! Responda apenas S ou N.')
+        print('Erro! Por favor digite S ou N.')
     if resp == 'N':
-            break
+        break
+print('-=' * 30) 
+print(f'Ao todo foram cadastradas {len(galera)} pessoas.')
 media = soma / len(galera)
-print('-=' * 30)
-print(f'A) Ao todo tem {len(galera)} pessoas cadastradas.')
-print(f'B) A média de idade é de {media:5.2f} anos.') 
-print(f'C) As mulheres cadastradas foram: ', end='')
+print(f'A média de idade é de {media:5.2f} anos.')
+print('As mulheres cadastradas foram: ', end='')
 for p in galera:
-     if p ['sexo'] == 'F':
-          print(f'{p["nome"]} ', end='')
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]} ', end='')
 print()
-print('D) A lista de pessoas que estão acima da média: ')
+print(f'Lista das pessoas que estão em cima da média: ')
 for p in galera:
-     if p ['idade'] >= media:
-          print('    ')
-          for k, v in p.items():
-              print(f'{k} = {v}; ', end='')  
-          print()
-print('<< ENCERRADO >>')      
-            
+    if p ['idade'] >= media:
+        print('     ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<<< ENCERRADO >>>')
