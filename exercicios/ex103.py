57c10bfbc8d3b070f3c34f61b26e1de44957fd80
Ramon Rigonato <ramon.rigonato00@hotmail.com>
2025-08-12 10:55:46 -0300

    Revisão
    Revisão

--- exercicios/ex103.py
+++ exercicios/ex103.py
@@ -1,44 +1,18 @@
 jogador = dict()
-partidas = list()
-time = list()
-while True:
-    jogador.clear()
-    jogador ['nome'] = str(input('Nome do jogador: '))
-    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-    partidas.clear()
-    for c in range(0, tot):
-        partidas.append(int(input(f'   Quantos gols na partida {c+1}? ')))
-    jogador ['gols'] = partidas [:]
-    jogador ['total'] = sum(partidas)
-    time.append(jogador.copy())
-    while True:
-        resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
-        if resp in 'SN':
-            break
-        print('ERRO! Responda apenas S ou N.')
-    if resp == 'N':
-        break 
+partida = list()
+jogador ['nome'] = str(input('Nome do jogador: '))
+tot = int(input(f'Quantos jogos o {jogador["nome"]} jogou? '))
+for c in range(0, tot):
+    partida.append(int(input(f'Quantos gols na partida {c}? ')))
+jogador ['gols'] = partida[:]
+jogador ['total'] = sum(jogador['gols'])
 print('-=' * 30)
-print('cod ', end='')
-for i in jogador.keys():
-    print(f'{i:<15}', end='')
-print()
-print('-' * 40)
-for k, v in enumerate(time):
-    print(f'{k:>3} ', end='')
-    for d in v.values():
-        print(f'{str(d):<15}', end='')
-    print()
-print('-' * 40)
-while True:
-    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
-    if busca == 999:
-        break
-    if busca >= len(time):
-        print(f'ERRO! Não existe jogador com código {busca}!')
-    else:
-        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
-        for i, g in enumerate(time[busca]['gols']):
-            print(f'    No jogo {i+1} fez {g} gols.')
-    print('-' * 40)
-print('<<< VOLTE SEMRPE! >>>')
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o {v}')
+print('-=' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])}')
+for i, v in enumerate(jogador['gols']):
+    print(f'            => Na partida {i}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
